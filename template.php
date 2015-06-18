d57ad7a77760dab5374177ab07750fc46c95bbfd
Patrick McDermott <patnet2004@gmail.com>
2015-06-18 00:18:07 -0700

    2015-06-17@10:37pm tPic-fix5

--- template.php
+++ template.php
@@ -15,26 +15,45 @@ if(isset($_ENV['OPENSHIFT_REPO_DIR']))
 {
 	$file = $_ENV['OPENSHIFT_REPO_DIR'].$file;
 }
-
-echo($file);
 if(isset($_GET['tnum']) && is_file($file))
 {
 	$img = "<img src=\"tPic/".$_GET['tnum'].".jpg\" />"; 
 }
+$status = "";
+if(isset($completed) && $completed == "completed")
+{
+	$style = "style=\"background-color:green\"";
+	$status = "complete";
+}
+else
+{
+	$style = "style=\"background-color:red\"";
+	$status = "not complete";
+}
+if(isset($_GET['tnum']) && is_numeric($_GET['tnum']) && $_GET['tnum'] != "")
+{
+
 $GLOBALS['output'] = "
 
 <!DOCTYPE html>
 <html>
 <body>
 <p>
-Territory &#35;:{[tnum]}
+Territory &#35;: ".$_GET['tnum']."</br>
+Status: ".$status."
 </p>
-".$img."
 <p>
+<table>
+<tr>
+<td ".$style.">
+".$img."
+</td>
+</tr>
+</table>
 </p>
 </body>
 </html>
 
 ";
-
+}
 }
