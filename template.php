bbb91982e4d20a2f87f5879969dc222f7750d03e
Patrick McDermott <patnet2004@gmail.com>
2015-06-17 23:09:13 -0700

    2015-06-17@10:37pm tPic-fix

--- template.php
+++ template.php
@@ -7,6 +7,12 @@
 {
 $img = "";
 $file = "tPic/".$_GET['tNum'].".jpg";
+if(isset($_ENV['OPENSHIFT_DATA_DIR']))
+{
+	$file = $_ENV['OPENSHIFT_DATA_DIR']."/".$file;
+}
+
+echo($file);
 if(isset($_GET['tNum']) && is_file($file))
 {
 	$img = "<img src=\"tPic/".$_GET['tNum'].".jpg\" />"; 
