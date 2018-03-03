6a86652b38a89f342c360a59d164f57428c4d892
Patrick McDermott <patnet2004@gmail.com>
2018-03-02 17:22:51 -0800

    color change

--- index.php
+++ index.php
@@ -320,7 +320,7 @@ if(isset($GLOBALS['checkout'][$i+1]) && $GLOBALS['checkout'][$i+1])
 }
 else
 {
-	$statusColor = str_replace("<!--{[border]}-->","border:2px 	dashed grey;height:50px;width:50px;background-color:#ff9999;",$statusColor);
+	$statusColor = str_replace("<!--{[border]}-->","border:2px 	dashed grey;height:50px;width:50px;;opacity:0.75;",$statusColor);
 }
 
 
@@ -349,7 +349,7 @@ if(isset($GLOBALS['checkout'][$i+1]) && $GLOBALS['checkout'][$i+1])
 }
 else
 {
-	$statusColor = str_replace("<!--{[border]}-->","border:2px 	dashed grey;height:50px;width:50px;background-color:#ff9999;",$statusColor);
+	$statusColor = str_replace("<!--{[border]}-->","border:2px 	dashed grey;height:50px;width:50px;;opacity:0.75;",$statusColor);
 }
 
 
@@ -378,7 +378,7 @@ if(isset($GLOBALS['checkout'][$i+1]) && $GLOBALS['checkout'][$i+1])
 }
 else
 {
-	$statusColor = str_replace("<!--{[border]}-->","border:2px 	dashed grey;height:50px;width:50px;background-color:#ff9999;",$statusColor);
+	$statusColor = str_replace("<!--{[border]}-->","border:2px 	dashed grey;height:50px;width:50px;;opacity:0.75;",$statusColor);
 }
 
 
@@ -407,7 +407,7 @@ if(isset($GLOBALS['checkout'][$i+1]) && $GLOBALS['checkout'][$i+1])
 }
 else
 {
-	$statusColor = str_replace("<!--{[border]}-->","border:2px 	dashed grey;height:50px;width:50px;background-color:#ff9999;",$statusColor);
+	$statusColor = str_replace("<!--{[border]}-->","border:2px 	dashed grey;height:50px;width:50px;;opacity:0.75;",$statusColor);
 }
 
 
@@ -439,7 +439,7 @@ if(isset($GLOBALS['checkout'][$i+1]) && $GLOBALS['checkout'][$i+1])
 }
 else
 {
-	$statusColor = str_replace("<!--{[border]}-->","border:2px 	dashed grey;height:50px;width:50px;background-color:#ff9999;",$statusColor);
+	$statusColor = str_replace("<!--{[border]}-->","border:2px 	dashed grey;height:50px;width:50px;opacity:0.75;",$statusColor);
 }
 
 
@@ -470,7 +470,7 @@ if(isset($GLOBALS['checkout'][$i+1]) && $GLOBALS['checkout'][$i+1])
 }
 else
 {
-	$statusColor = str_replace("<!--{[border]}-->","border:2px 	dashed grey;height:50px;width:50px;background-color:#ff9999;",$statusColor);
+	$statusColor = str_replace("<!--{[border]}-->","border:2px 	dashed grey;height:50px;width:50px;;opacity:0.75;",$statusColor);
 }
 
 
@@ -501,7 +501,7 @@ if(isset($GLOBALS['checkout'][$i+1]) && $GLOBALS['checkout'][$i+1])
 }
 else
 {
-	$statusColor = str_replace("<!--{[border]}-->","border:2px 	dashed grey;height:50px;width:50px;background-color:#ff9999;",$statusColor);
+	$statusColor = str_replace("<!--{[border]}-->","border:2px 	dashed grey;height:50px;width:50px;;opacity:0.75;",$statusColor);
 }
 
 
@@ -532,7 +532,7 @@ if(isset($GLOBALS['checkout'][$i+1]) && $GLOBALS['checkout'][$i+1])
 }
 else
 {
-	$statusColor = str_replace("<!--{[border]}-->","border:2px 	dashed grey;height:50px;width:50px;background-color:#ff9999;",$statusColor);
+	$statusColor = str_replace("<!--{[border]}-->","border:2px 	dashed grey;height:50px;width:50px;;opacity:0.75;",$statusColor);
 }
 
 
